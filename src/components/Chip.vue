fd86b9023a6c3ec238c4ae6b0cb4f97d80f8df0b
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-02-22 16:14:45 +0100

    Add a chip component

---
+++ src/components/Chip.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="md-chip" :title="title">
+      <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Chip",
+    props: ['title']
+}
+</script>
+
+<style lang="scss" scoped>
+$md-chip-height: 32px;
+.md-chip {
+  display: inline-block;
+  background: hsl(var(--cardColorPrimary-h), var(--cardColorPrimary-s), calc(var(--cardColorPrimary-l) - 10%));
+  font-family: var(--sansSerifFont);
+  padding: 0 12px;
+  border-radius: 32px;
+  font-size: 13px;
+  font-weight: bold;
+  color: hsl(var(--cardColorPrimary-h), 0% , calc(100% - calc(var(--cardColorPrimary-l) - 10%)));
+  &:hover {
+    background: hsl(var(--cardColorPrimary-h), var(--cardColorPrimary-s), calc(var(--cardColorPrimary-l) - 30%));
+    color: hsl(var(--cardColorPrimary-h), 0% , calc(100% - calc(var(--cardColorPrimary-l) + 20%)));
+  }
+  text-transform: capitalize;
+}
+
+.md-chip, .md-chip-icon {
+  height: $md-chip-height;
+  line-height: $md-chip-height;
+}
+</style>
